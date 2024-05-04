9317e9ff0b7bfcd4fecf465aeb3766b8c490537a
Junior Paradelo <juniorparadelo@gmail.com>
2024-05-04 15:49:00 +0200

    startup project

---
+++ src/main/java/k3ras/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package k3ras.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaApplication.class, args);
+    }
+
+}
