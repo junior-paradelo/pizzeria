a4bd3cf1493f248cae301b41e62dc2d9304be385
Junior Paradelo <juniorparadelo@gmail.com>
2024-05-06 12:40:18 +0200

    feat: sorting in repository

--- src/main/java/k3ras/pizzeria/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/k3ras/pizzeria/persistence/repository/PizzaPagSortRepository.java
@@ -1,9 +1,12 @@
 package k3ras.pizzeria.persistence.repository;
 
 import k3ras.pizzeria.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
 }
--- src/main/java/k3ras/pizzeria/web.controller/PizzaController.java
+++ src/main/java/k3ras/pizzeria/web.controller/PizzaController.java
@@ -25,8 +25,11 @@ public class PizzaController {
     }
 
     @GetMapping("/available")
-    public ResponseEntity<List<PizzaEntity>> getAvailable() {
-        return ResponseEntity.ok(this.pizzaService.getAvailable());
+    public ResponseEntity<Page<PizzaEntity>> getAvailable(@RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "5") int elements,
+                                                          @RequestParam(defaultValue = "price") String sortBy,
+                                                          @RequestParam(defaultValue = "ASC") String sortDirection) {
+        return ResponseEntity.ok(this.pizzaService.getAvailable(page, elements, sortBy, sortDirection));
     }
 
     @GetMapping("/name/{name}")
