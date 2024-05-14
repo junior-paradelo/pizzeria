a3e8a06e96dedeba3eca58b34e6149765dfa7943
Junior Paradelo <juniorparadelo@gmail.com>
2024-05-14 18:47:52 +0200

    feat: add requestmatchers

--- src/main/java/k3ras/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/k3ras/pizzeria/web/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package k3ras.pizzeria.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,9 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(customizeRequests -> customizeRequests.anyRequest().authenticated())
-                .csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(customRequest -> customRequest
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
         return http.build();
     }
 }
